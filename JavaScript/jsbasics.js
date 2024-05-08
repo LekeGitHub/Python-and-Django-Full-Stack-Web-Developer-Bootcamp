627bed53b5bbc9313c1d7b99a4de5b27fbecf10b
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-05-08 17:24:53 +0100

    ....

---
+++ JavaScript/jsbasics.js
@@ -0,0 +1,43 @@
+a = 2+3
+alert("Hello Leke")
+undefined
+// Comment in java script.
+undefined
+// Basic data type
+undefined
+10
+10
+20.2
+20.2
+-13.4
+-13.4
+// Strings
+undefined
+"Hello World!"
+'Hello World!'
+"10"
+'10'
+//Booleans
+undefined
+true
+true
+false
+false
+// undefined and null
+undefined
+2+2
+4
+10/2
+5
+2/5
+0.4
+2%5
+2
+2 ** 5
+32
+6 %2
+0
+6%4
+2
+// Strings
+undefined
---
+++ JavaScript/myscript.js
@@ -0,0 +1,7 @@
+alert("Welcome to your bank");
+
+var deposite = prompt("How much will you like to deposite today? ")
+
+alert("You've deposited: "+deposite)
+
+console.log("You are a cool person!");
