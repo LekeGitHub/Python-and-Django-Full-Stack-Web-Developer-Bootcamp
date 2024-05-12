7e77b88421c1528d1b33b236dac18118726c8799
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-05-12 22:30:48 +0100

    ....

--- JavaScript/loop.js
+++ JavaScript/loop.js
@@ -1,8 +1,14 @@
-var x = 0;
+var x = 1;
 
-while(x < 5){
-    console.log("x = "+x);
-    console.log("adding 1 to x ");
+while(x <= 10){
+    // console.log("x = "+x);
+    
+    if (x % 2 === 0){
+        console.log(x);
+        // break;
+    }
+    
+    // console.log("adding 1 to x ");
     
     x = x+1;
 }
