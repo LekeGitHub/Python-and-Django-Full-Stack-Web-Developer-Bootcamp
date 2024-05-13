dc96c0d108c417532a9161f901ab7a4ebb4691e2
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-05-14 00:49:00 +0100

    ....

--- JavaScript/forloop.js
+++ JavaScript/forloop.js
@@ -8,8 +8,61 @@
 //    console.log(word[i]);
 // }
 
-var word = "ABABABABAB"
+// var word = "ABABABABAB"
 
-for (var i = 0; i < word.length; i=i+2){
-   console.log(word[i])
+// for (var i = 0; i < word.length; i=i+2){
+//    console.log(word[i])
+// }
+
+// var word = 0
+
+// while(word < 5){
+//    console.log("Hello!");
+//    word++
+// }
+
+// for (var i = 0; i < 5; i++){
+//    console.log("Hello!");
+// }
+
+
+// Print out odd numbers from 1 - 25
+// using while loop
+
+// var num = 1;
+
+// while(num < 25){
+//    if (num % 2 !== 0){
+//       console.log(num)
+//    }
+//    num++
+// }
+
+// Print out odd numbers from 1 - 25
+// using while forloop
+
+// for (var i = 0; i < 25; i++){
+//    if (i % 2 !== 0){
+//       console.log(i);
+//    }
+// }
+
+var fname = prompt("Please enter your first name")
+var lname = prompt("Please enter your last name")
+var age = prompt("How old are you? ")
+hieght = prompt("Please enter your hieght: ")
+var pet = prompt("What is your pet name? ")
+alert("Thank you! thats it.")
+
+
+if (fname[0] == lname[0] && age >= 20 || age <= 30 && hieght >= 170 && pet[-0] == 'y'){
+   console.log("Hello comrade!, You passed the spy test");
+}else{
+   console.log("Sorry nothing to see here")
 }
+
+
+
+// for(var i; i < fname.length; i++){
+
+// }
