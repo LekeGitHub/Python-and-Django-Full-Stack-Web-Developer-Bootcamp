9ca13b4a50f41993872875ac260b39f5f849fa00
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-05-14 00:01:33 +0100

    ....

--- JavaScript/forloop.js
+++ JavaScript/forloop.js
@@ -2,8 +2,14 @@
 //     console.log("Number is "+i )
 // }
 
-var word = "ABCDEFGHIJK"
+// var word = "ABCDEFGHIJK"
 
-for (var i = 0; i < word.length; i++) {
-   console.log(word[i]);
+// for (var i = 0; i < word.length; i++) {
+//    console.log(word[i]);
+// }
+
+var word = "ABABABABAB"
+
+for (var i = 0; i < word.length; i=i+2){
+   console.log(word[i])
 }
