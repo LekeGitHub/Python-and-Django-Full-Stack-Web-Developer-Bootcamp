6bb21dadd1c454678a83f54e19cb58ad65fab35d
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-05-19 14:48:16 +0100

    >>>>

--- JavaScript/myscript.js
+++ JavaScript/myscript.js
@@ -5,3 +5,22 @@
 // alert("You've deposited: "+deposite)
 
 // console.log("You are a cool person!");
+
+function hello(name){
+    console.log("Hello "+name)
+}
+
+// Addition 
+function addNum(n1,n2){
+    console.log(n1+n2);
+}
+
+// Default Parameters in function
+function greetUser(name="Leke"){
+    console.log("Hello "+name);
+}
+
+// Returning input out of function and asigning it to a variable outside the fuction
+function formal(title="Sir", name='Sam'){
+    return title + " " + name;
+}
