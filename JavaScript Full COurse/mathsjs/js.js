c74b10ac311fb46ae77e35af0278cdad87fa14f2
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-08-04 07:39:49 +0100

    More JavaScript lessons

---
+++ JavaScript Full COurse/mathsjs/js.js
@@ -0,0 +1,27 @@
+// Maths = built in object that provides a 
+//         collection of properties and methods
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+
+let x = 7.91;
+let y = 2.9;
+let z = 1;
+
+// z = Math.round(x + y);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(x);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+console.log(min)
