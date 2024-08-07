5e42b43eef69e289191613a5c9c74745621b064e
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-08-07 15:33:58 +0100

    Adding more JavaScript - Functions

--- JavaScript Full COurse/functions/js.js
+++ JavaScript Full COurse/functions/js.js
@@ -20,3 +20,13 @@ function add(x, y){
 
 let answer = add(2,3)
 console.log(answer)
+
+function subtract(x, y){
+    return x - y
+}
+
+function multiply(x, y){
+    return x * y
+}
+
+console.log(multiply(10, 6));
