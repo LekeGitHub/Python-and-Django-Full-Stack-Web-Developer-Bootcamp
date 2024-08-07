1e9737ec6631cac425428d322def86d68bf9b2ec
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-08-07 15:16:20 +0100

    Adding more JavaScript - Functions

---
+++ JavaScript Full COurse/functions/js.js
@@ -0,0 +1,9 @@
+// Function = A section of reusable code.
+//            Declare code once, use it whenever you want.
+//            Call the function to execute that code
+
+
+function happyBirthday(){
+    
+}
+
