fd79c77ddb0e80c375aa843ada98518120a8d676
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-08-07 15:30:49 +0100

    Adding more JavaScript - Functions

--- JavaScript Full COurse/functions/js.js
+++ JavaScript Full COurse/functions/js.js
@@ -3,12 +3,20 @@
 //            Call the function to execute that code
 
 
-function happyBirthday(username, age){
-    console.log("Happy Birthday to you");
-    console.log(`Happy Birthday to you ${username}`);
-    console.log(`You are ${age}`);
-    ;
+// function happyBirthday(username, age){
+//     console.log("Happy Birthday to you");
+//     console.log(`Happy Birthday to you ${username}`);
+//     console.log(`You are ${age}`);
+//     ;
+// }
+
+// happyBirthday("Leke", 22);
+// happyBirthday("SpongeBob", 30);
+
+function add(x, y){
+    let result = x + y;
+    return result;
 }
 
-happyBirthday("Leke", 22);
-happyBirthday("SpongeBob", 30);
+let answer = add(2,3)
+console.log(answer)
