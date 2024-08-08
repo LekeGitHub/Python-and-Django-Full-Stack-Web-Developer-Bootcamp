fc321c3e313576f4d3575e7cdd0ad760064cc9e0
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-08-08 07:03:21 +0100

    Adding more JavaScript - Array - spread operator

---
+++ JavaScript Full COurse/functions/restparam.js
@@ -0,0 +1,49 @@
+// Rest Parameters = (...rest) allow a function work a variable 
+//                   number if arguments by building them into an array
+
+//                   spread = expands an array into seperate elements
+//                   rest = bundles seperate elements into an array
+
+
+// function openFridge(...foods){
+//     console.log(...foods)
+// }
+
+// const food1 = "pizza";
+// const food2 = "hamburger";
+// const food3 = "hotdog";
+// const food4 = "sushi";
+// const food5 = "ramen";
+
+// openFridge(food1, food2, food3, food4, food5);
+
+
+// function getFood(...foods){
+//     return foods;
+// }
+
+// const foods = getFood(food1, food2, food3, food4, food5);
+// console.log(foods);
+
+
+// function sum(...numbers){
+//     let result = 0;
+//     for(let number of numbers){
+//         result += number;
+//     }
+//     return result;
+// }
+
+// const total = sum(1, 2, 3, 4, 5);
+// console.log(`Your total is $${total}`)
+
+// function getAverage(...numbers){
+//     let result = 0;
+//     for(let number of numbers){
+//         result += number;
+//     }
+//     return result / numbers.length;
+// }
+
+// const total = getAverage(80, 60, 78, 96, 90);
+// console.log(`Your average is ${total}`);
