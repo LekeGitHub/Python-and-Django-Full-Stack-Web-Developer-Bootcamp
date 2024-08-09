7359b1447b22d92d76082d8588a3cabf0698e649
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-08-09 23:02:29 +0100

    Adding Java SCript - Dice Roller Game

---
+++ JavaScript Full COurse/Games/dice rolling game/js.js
@@ -0,0 +1,21 @@
+
+
+function rollDice(){
+    const numOFDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOFDice; i++){
+        const value = Math.floor(Math.random() *6) + 1;
+        
+        values.push(value);
+        
+        images.push(`<img src="img/${value}.png" alt="Dice ${value}">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+}
