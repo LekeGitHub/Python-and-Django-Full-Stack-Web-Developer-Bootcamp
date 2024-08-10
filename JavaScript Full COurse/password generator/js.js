d579cb29ece85c6fb8db3a41878a83fafa9b0701
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-08-10 11:47:06 +0100

    Adding Java SCript - Password Generator

---
+++ JavaScript Full COurse/password generator/js.js
@@ -0,0 +1,21 @@
+
+
+
+function passwordGenerator(passwordLength, isUppercase, isLowercase, isNumbers, isSymbols){
+
+    return "";
+}
+
+const passwordLength = 12;
+const isUppercase = true;
+const isLowercase = true;
+const isNumbers = true;
+const isSymbol = true;
+
+const password = passwordGenerator(passwordLength, 
+                                   isUppercase, 
+                                   isLowercase, 
+                                   isNumbers, 
+                                   isSymbols);
+
+console.log(`Generated Password: ${password}`);
