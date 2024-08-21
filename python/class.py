760e3ddc24a15fb91474386ec0fc803ccf81a778
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-08-21 23:27:32 +0100

    adding class file to py

---
+++ python/class.py
@@ -0,0 +1,16 @@
+# class Dog():
+
+#     specie = "mammal"
+    
+#     def __init__(self, breed, name):
+#         self.breed = breed
+       
+#         self.name = name
+
+
+# myDog = Dog(breed = "lab", name = "sammy")
+
+
+# print(myDog.breed)
+# print(myDog.name)
+# print(myDog.specie)
---
+++ python/function.py
@@ -0,0 +1,35 @@
+# arrayCheck([1, 2, 3, 1,]) -> True
+# arrayCheck([1, 2, 3, 4, 1]) -> False
+# arrayCheck([1, 1, 2, 1, 2, 3,]) -> True
+
+# x = 25
+
+# def my_func():
+#     x = 50
+#     return x
+
+
+# my_func()
+# print(x)
+# print(my_func())
+
+# name = 'This is a global name'
+
+# def greet():
+#     name = "Sammy"
+
+#     def hello():
+#         print("Hello "+ name)
+    
+#     hello()
+
+
+# greet()
+
+# Class
+
+class Sample():
+    pass
+
+x = Sample()
+print(type(x))
