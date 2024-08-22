ada0690e184200b144ee878d89cce6881186c139
Adetoroye Oluleke Moses <adetleke@gmail.com>
2024-08-22 11:01:45 +0100

    ....

--- python/class.py
+++ python/class.py
@@ -16,21 +16,48 @@
 # print(myDog.specie)
 
 
-class Circle():
-    pi = 3.14
+# class Circle():
+#     pi = 3.14
 
-    def __init__(self, radius=1):
-        self.radius = radius
+#     def __init__(self, radius=1):
+#         self.radius = radius
     
-    def area(self):
-        return self.radius * self.radius * Circle.pi
+#     def area(self):
+#         return self.radius * self.radius * Circle.pi
     
-    def set_radius(self,new_r):
-        self.radius = new_r
+#     def set_radius(self,new_r):
+#         self.radius = new_r
 
 
 
-myc = Circle(3)
-myc.set_radius(900)
+# myc = Circle(3)
+# myc.set_radius(900)
 
-print(myc.area())
+# print(myc.area())
+
+
+class Animal():
+
+    def __init__(self) -> None:
+        print("Animal Created")
+
+    def whoAmI(self):
+        print("Animal")
+    
+    def eat(self):
+        print("Eating")
+
+class Dog(Animal):
+    def __init__(self) -> None:
+        print("Dog Created")
+
+    def bark(self):
+        print("WoOf!!!")
+    
+    def eat(self):
+        print("Dog is Eating")
+
+myDog = Dog()
+myDog.whoAmI()
+myDog.eat()
+myDog.bark()
